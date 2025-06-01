263e5c41bc9a5ed1479dc1b2fa168bdbbfeae337
Dre <92935512+JPAVictoria@users.noreply.github.com>
2025-06-01 13:17:42 +0800

    fix og-image commit

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -28,7 +28,7 @@ export const metadata: Metadata = {
     siteName: "Foodopedia",
     images: [
       {
-        url: "https://yourapp.com/og-image.jpg",
+        url: "https://yourapp.com/og-image.png",
         width: 1200,
         height: 630,
         alt: "My App",
